8fc4c095962d54b71cd25c127eb8c7851b77d8a7
zobayertihan <zobayertihan@gmail.com>
2022-11-25 19:29:22 +0600

    private buyer route created

--- index.js
+++ index.js
@@ -90,6 +90,13 @@ async function run() {
             const user = await usersCollection.findOne(query);
             res.send({ isAdmin: user?.role === 'admin' });
         })
+
+        app.get('/users/buyer/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email }
+            const user = await usersCollection.findOne(query);
+            res.send({ isBuyer: user?.role === 'buyer' });
+        })
     }
     finally {
 
